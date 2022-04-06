9e3b87b90ae84ae6f6ef21f6a2349b7271c98ed5
mmahelon <mmahelon@ucsc.edu>
2022-04-06 16:16:31 -0700

    "Initial Commit"

---
+++ src/scenes/Menu.js
@@ -0,0 +1,9 @@
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene");
+    }
+
+    create() {
+        this.addEventListener.text(20, 20, "Rocket Patrol Menu");
+    }
+}
