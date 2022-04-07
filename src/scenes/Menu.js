e0a2c3502301d957e625c43d77c63778dd8d2542
mmahelon <mmahelon@ucsc.edu>
2022-04-06 17:58:30 -0700

    "Added Assets"

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,6 +4,7 @@ class Menu extends Phaser.Scene {
     }
 
     create() {
-        this.addEventListener.text(20, 20, "Rocket Patrol Menu");
+        this.add.text(20, 20, "Rocket Patrol Menu");
+        this.scene.start("playScene");
     }
 }
